992665521790346e68eeb2328659845760bd77da
Waldemar Neto <waldemarnt@gmail.com>
2024-05-25 12:10:27 -0300

    feat: database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,16 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from '@src/persistence/prisma/prisma.service';
 import { ContentController } from '@src/http/rest/controller/content.controller';
 import { ContentManagementService } from '@src/core/service/content-management.service';
 import { MediaPlayerService } from '@src/core/service/media-player.service';
 import { ContentRepository } from '@src/persistence/repository/content.repository';
 import { VideoRepository } from '@src/persistence/repository/video.repository';
 import { MediaPlayerController } from '@src/http/rest/controller/media-player.controller';
-import { ConfigModule } from '@src/infra/module/config/config.module';
+import { PersistenceModule } from '@src/persistence/persistence.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [PersistenceModule.forRoot()],
   controllers: [ContentController, MediaPlayerController],
   providers: [
-    PrismaService,
     ContentManagementService,
     MediaPlayerService,
     ContentRepository,
