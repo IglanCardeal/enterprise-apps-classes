df4a50f5b58f8b7d1b6ea995273154e82a89bfd3
Waldemar Neto <waldemarnt@gmail.com>
2024-05-25 16:33:58 -0300

    feat: finish core

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ContentController } from '@src/http/rest/controller/content.controller';
 import { ContentManagementService } from '@src/core/service/content-management.service';
 import { MediaPlayerService } from '@src/core/service/media-player.service';
 import { ContentRepository } from '@src/persistence/repository/content.repository';
 import { VideoRepository } from '@src/persistence/repository/video.repository';
 import { MediaPlayerController } from '@src/http/rest/controller/media-player.controller';
 import { PersistenceModule } from '@src/persistence/persistence.module';
+import { VideoUploadController } from '@src/http/rest/controller/video-upload.controller';
 
 @Module({
   imports: [PersistenceModule.forRoot()],
-  controllers: [ContentController, MediaPlayerController],
+  controllers: [VideoUploadController, MediaPlayerController],
   providers: [
     ContentManagementService,
     MediaPlayerService,
