2e60e62d5b09af61c2c51a5eb5c8faac8aa24c4e
IglanCardeal <cmtcardeal@outlook.com>
2024-06-14 22:00:39 -0300

    feat: improve video upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ContentController } from '@src/http/rest/controller/content.controller';
 import { ContentManagementService } from '@src/core/service/content-management.service';
 import { MediaPlayerService } from '@src/core/service/media-player.service';
 import { ContentRepository } from './persistence/repository/content.repository';
 import { VideoRepository } from './persistence/repository/video.repository';
 import { MediaPlayerController } from './http/rest/controller/media-player.controller';
 import { PersistenceModule } from './persistence/persistence.module';
+import { VideoUploadController } from './http/rest/controller/video-upload.controller';
 
 @Module({
   imports: [PersistenceModule.forRoot()],
-  controllers: [ContentController, MediaPlayerController],
+  controllers: [VideoUploadController, MediaPlayerController],
   providers: [
     ContentManagementService,
     MediaPlayerService,
