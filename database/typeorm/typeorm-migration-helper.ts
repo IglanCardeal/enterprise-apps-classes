d3988e704ee3f362bcf818f217aebf9033d507e8
Waldemar Neto <waldemarnt@gmail.com>
2024-08-03 17:18:12 -0300

    feat: create content module

--- database/typeorm/typeorm-migration-helper.ts
+++ database/typeorm/typeorm-migration-helper.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@src/infra/module/config/service/config.service';
-import { TypeOrmMigrationService } from '@src/infra/module/typeorm/service/typeorm-migration.service';
-import { PersistenceModule } from '@src/persistence/persistence.module';
+import { ConfigService } from '@contentModule/infra/module/config/service/config.service';
+import { TypeOrmMigrationService } from '@contentModule/infra/module/typeorm/service/typeorm-migration.service';
 import { DataSourceOptions } from 'typeorm';
 import { createPostgresDatabase } from 'typeorm-extension';
+import { PersistenceModule } from '@contentModule/persistence/persistence.module';
 
 const createDatabaseModule = async () => {
   return await NestFactory.createApplicationContext(
